8fe463935a65c00807288a7f7ae88417d10039fb
comrade001 <abrahamsa@live.com.mx>
2019-07-08 16:59:45 -0500

    Revisión 08/07/2019 +Completo parse de Mensaje Tipo 2 +Queda pendiente revisar Mensaje Tipo 0, 4 y 7

--- src/socketlistener/Executor1Thread.java
+++ src/socketlistener/Executor1Thread.java
@@ -46,104 +46,138 @@ class UdpUnicastServer implements Runnable {
     @Override
     public void run() {
         try (DatagramSocket socket = new DatagramSocket(clientPort)) {
-        byte[] buffer = new byte[1024];    
-        DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
-        int count = 0;
-        while(true){
-            socket.receive(peticion);
-            StringBuilder sb = new StringBuilder();
-            for (byte b : buffer) {
-                sb.append(String.format("%02X", b));
-            }
+            
+            int count = 0;
+            while(true){
+                byte[] buffer = new byte[1024];    
+                DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+                socket.receive(peticion);
+                StringBuilder sb = new StringBuilder();
+                for (byte b : buffer) {
+                    sb.append(String.format("%02X", b));
+                }
 
-            String mensaje = sb.toString();
-            int carrete = 0;
-            Integer OptionsByte = Integer.parseInt(
-                    mensaje.substring(0, carrete += BYTE), 16);
-            
-            int numOfBits = 8;
-            boolean[] OptionsByteBoolean = new boolean[numOfBits];
-            for(int i = 0; i< numOfBits; i++){
-                OptionsByteBoolean[i] = (OptionsByte & 1 << i) != 0;
-            }
-            
-            if(OptionsByteBoolean[0]){
-                Integer MobileIdLength = Integer.parseInt(
+                String mensaje = sb.toString();
+                System.out.println("Input " + count + " : " + mensaje);
+                int carrete = 0;
+                Integer OptionsByte = Integer.parseInt(
+                        mensaje.substring(0, carrete += BYTE), 16);
+
+                int numOfBits = 8;
+                boolean[] OptionsByteBoolean = new boolean[numOfBits];
+                for(int i = 0; i< numOfBits; i++){
+                    OptionsByteBoolean[i] = (OptionsByte & 1 << i) != 0;
+                }
+
+                Integer MobileIdLength = null;
+                String MobileId = null;
+                if(OptionsByteBoolean[0]){
+                    MobileIdLength = Integer.parseInt(
+                            mensaje.substring(carrete, carrete += BYTE), 16);
+                    MobileId = mensaje.substring(carrete
+                            , carrete += MobileIdLength * BYTE);
+                }
+                Integer MobileIdTypeLength = null;
+                String MobileIdType = null;
+                if(OptionsByteBoolean[1]){
+                    MobileIdTypeLength = Integer.parseInt(
+                            mensaje.substring(carrete, carrete += BYTE), 16);
+    //                Integer MobileIdType = Integer.parseInt(
+    //                        mensaje.substring(
+    //                                carrete, carrete += MobileIdTypeLength * BYTE));
+                    MobileIdType = mensaje.substring(
+                                    carrete, carrete += MobileIdTypeLength * BYTE);
+                }
+
+                String ServiceType = mensaje.substring(carrete, carrete += BYTE);
+                String MessageType = mensaje.substring(carrete, carrete += BYTE);
+                Integer SequenceNumber = Integer.parseInt(
+                        mensaje.substring(carrete, carrete += 2 * BYTE), 16); 
+                
+                System.out.println("SequenceNumber " + SequenceNumber);
+
+                java.util.Date UpdateTime = new java.util.Date(
+                        Long.parseLong(mensaje.substring(
+                                        carrete, carrete += 4 * BYTE), 16) * 1000);
+
+                java.util.Date TimeOfFix = new java.util.Date(
+                        Long.parseLong(mensaje.substring(
+                                        carrete, carrete += 4 * BYTE), 16) * 1000);
+
+                double Latitude = (int) Long.parseLong(mensaje.substring(
+                                        carrete, carrete += 4 * BYTE), 16) / 1e7;
+
+                double Longitude = (int) Long.parseLong(mensaje.substring(
+                                        carrete, carrete += 4 * BYTE), 16) / 1e7;
+
+                double Altitude = (int) Long.parseLong(mensaje.substring(
+                                        carrete, carrete += 4 * BYTE), 16);
+
+                double Speed = (int) Long.parseLong(mensaje.substring(
+                        carrete, carrete += 4 * BYTE), 16) * 0.036;
+
+                Integer Heading = (int) Integer.parseInt(mensaje.substring(
+                        carrete, carrete += 2 * BYTE), 16);
+
+                Integer Satellites = Integer.parseInt(
                         mensaje.substring(carrete, carrete += BYTE), 16);
-                String MobileId = mensaje.substring(carrete
-                        , carrete += MobileIdLength * BYTE);
-            }
-            if(OptionsByteBoolean[1]){
-                Integer MobileIdTypeLength = Integer.parseInt(
+
+                Integer FixStatus = Integer.parseInt(
                         mensaje.substring(carrete, carrete += BYTE), 16);
-                Integer MobileIdType = Integer.parseInt(
-                        mensaje.substring(
-                                carrete, carrete += MobileIdTypeLength * BYTE));
+
+                boolean[] FixStatusBoolean = new boolean[numOfBits];
+                for(int i = 0; i< numOfBits; i++){
+                    FixStatusBoolean[i] = (FixStatus & 1 << i) != 0;
+                }
+
+                Integer Carrier = Integer.parseInt(
+                        mensaje.substring(carrete, carrete += 2 * BYTE), 16);
+
+                short RSSI = (short) Integer.parseInt(mensaje.substring(
+                                        carrete, carrete += 2 * BYTE), 16);
+
+                Integer CommState = Integer.parseInt(
+                        mensaje.substring(carrete, carrete += BYTE), 16);
+
+                boolean[] CommStateBoolean = new boolean[numOfBits];
+                for(int i = 0; i< numOfBits; i++){
+                    CommStateBoolean[i] = (CommState & 1 << i) != 0;
+                }
+
+                InetAddress address = peticion.getAddress();
+                
+                String response = String.format("%02X", OptionsByte) 
+                        + String.format("%02X", MobileIdLength)
+                        + MobileId 
+                        + String.format("%02X", MobileIdTypeLength)
+                        + MobileIdType 
+                        + "02" //ServiceType
+                        + "01" //MessageType
+                        + String.format("%04X", SequenceNumber)
+                        + MessageType + "00" + "00" + "000000";
+                System.out.println("Output " + count + " : " + mensaje);
+                int port = peticion.getPort();
+                byte[] b = hexStringToByteArray(response);
+                peticion = new DatagramPacket(b
+                        , b.length, address, port);
+                socket.send(peticion);
             }
-            
-            String ServiceType = mensaje.substring(carrete, carrete += BYTE);
-            String MessageType = mensaje.substring(carrete, carrete += BYTE);
-            Integer SequenceNumber = Integer.parseInt(
-                    mensaje.substring(carrete, carrete += 2 * BYTE), 16); 
-            
-            java.util.Date UpdateTime = new java.util.Date(
-                    Long.parseLong(mensaje.substring(
-                                    carrete, carrete += 4 * BYTE), 16) * 1000);
-            
-            java.util.Date TimeOfFix = new java.util.Date(
-                    Long.parseLong(mensaje.substring(
-                                    carrete, carrete += 4 * BYTE), 16) * 1000);
-            
-            double Latitude = (int) Long.parseLong(mensaje.substring(
-                                    carrete, carrete += 4 * BYTE), 16) / 1e7;
-            
-            double Longitude = (int) Long.parseLong(mensaje.substring(
-                                    carrete, carrete += 4 * BYTE), 16) / 1e7;
-            
-            double Altitude = (int) Long.parseLong(mensaje.substring(
-                                    carrete, carrete += 4 * BYTE), 16);
-            
-            double Speed = (int) Long.parseLong(mensaje.substring(
-                    carrete, carrete += 4 * BYTE), 16) * 0.036;
-            
-            Integer Heading = (int) Integer.parseInt(mensaje.substring(
-                    carrete, carrete += 2 * BYTE), 16);
-            
-            Integer Satellites = Integer.parseInt(
-                    mensaje.substring(carrete, carrete += BYTE), 16);
-            
-            Integer FixStatus = Integer.parseInt(
-                    mensaje.substring(carrete, carrete += BYTE), 16);
-            
-            boolean[] FixStatusBoolean = new boolean[numOfBits];
-            for(int i = 0; i< numOfBits; i++){
-                FixStatusBoolean[i] = (FixStatus & 1 << i) != 0;
-            }
-            
-            Integer Carrier = Integer.parseInt(
-                    mensaje.substring(carrete, carrete += 2 * BYTE), 16);
-            
-            short RSSI = (short) Integer.parseInt(mensaje.substring(
-                                    carrete, carrete += 2 * BYTE), 16);
-            
-            Integer CommState = Integer.parseInt(
-                    mensaje.substring(carrete, carrete += BYTE), 16);
-            
-            System.out.println("Count " + count + " : " + mensaje);
-            
-            InetAddress address = peticion.getAddress();
-            String response = "83084562247869010102010006";
-            int port = peticion.getPort();
-            peticion = new DatagramPacket(response.getBytes()
-                    , response.getBytes().length, address, port);
-            socket.send(peticion);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (SocketException e) {
-        e.printStackTrace();
-    } catch (UnknownHostException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-  }
+    
+    public static byte[] hexStringToByteArray(String s) {
+        byte[] b = new byte[s.length() / 2];
+        for (int i = 0; i < b.length; i++) {
+          int index = i * 2;
+          int v = Integer.parseInt(s.substring(index, index + 2), 16);
+          b[i] = (byte) v;
+        }
+        return b;
+    }
 }
